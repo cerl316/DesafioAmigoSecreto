e88052def13d10c4b67fe094eaef68c3b4f2745e
Cristian <validacerl316@gmail.com>
2025-03-17 11:56:02 -0300

    terminado con listas, mensajes y validacion

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let NombreSecreto ;
+let NombreSecreto = ' ';
 let ArrayNombres = [];
-//let numeroMaximo = 10;
+
 
 
 
@@ -11,18 +11,37 @@ let ArrayNombres = [];
 
 function agregarAmigo() {
     let NombreSecreto = document.getElementById('amigo').value;
-    ArrayNombres.push(NombreSecreto);
-    console.log(NombreSecreto);
-    lista();
-    limpiar();
+    if (NombreSecreto==""){
+        alert("Nombre Ingresado no Debe estar en Blanco");
+    }
+    else {
+        ArrayNombres.push(NombreSecreto);
+
+        for (let i=0; i< ArrayNombres.length;i++) {
+        }
+                let elem=document.createElement("Li");
+                let arrNom=NombreSecreto;
+                elem.innerHTML=arrNom;
+                document.getElementById("listaAmigos").appendChild(elem);
+      
+        limpiar();
+    }
+   
 }
+ 
+
 
 function limpiar(){
     document.getElementById('amigo').value = '';
 }
 
-function lista(){
-
-
+function sortearAmigo(){
+    if (ArrayNombres!=[]){
+        let nroIndice = Math.floor(Math.random()*ArrayNombres.length+1);
+        alert(ArrayNombres[nroIndice]);
+        ArrayNombres.splice(nroIndice,1);
+    }
+    else{
+        alert("Ya No Existen Amigos que Sortear :(");
+    }
 }
-
